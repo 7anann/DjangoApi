7b54b439aa057a857b2dd135135e23de87526593
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2022-02-10 22:34:36 +0200

    Update urls.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from apiApp.views import loginView
 from apiApp.views import *
 from apiApp import views
 router = routers.DefaultRouter()
@@ -27,5 +28,6 @@ router.register(r'tags', views.TagViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('login/', loginView.as_view())
 ]
