0d9c4c37474ce40191ae76009761895f10ed2cc3
Hanan Mohamed <36165902+7anann@users.noreply.github.com>
2022-02-10 22:38:38 +0200

    Update serializers.py

--- api/apiApp/serializers.py
+++ api/apiApp/serializers.py
@@ -4,9 +4,33 @@ from .models import *
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+        def create(self, validated_data):
+        user = myuser.objects.create(
+            Email=validated_data['Email'],
+            password=validated_data['password'],
+        )
+
+        return user
     class Meta:
         model = myuser
         exclude = ['fb_account', 'birthdate', 'country', 'is_active']
+
+class Login(serializers.HyperlinkedModelSerializer):
+    #user = UserSerializer()
+    class Meta:
+        model = myuser
+        fields = ('Email', 'password')
+
+    def validate(self, data):  # validated_data
+        email = data.get('Email')
+        passw = data.get('password')
+        user = myuser.objects.filter(Email=email, password=passw).distinct()
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("Invalid Email address or password")
+
+        return user_obj
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
